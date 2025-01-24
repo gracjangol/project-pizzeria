abc1765c6091c87f05e6ceeda10278c1df0a08ff
Gracjan Gołębiewski <gra.golebiewski@gmail.com>
2025-01-24 15:42:59 +0100

    generate booking page

---
+++ src/js/components/Booking.js
@@ -0,0 +1,31 @@
+import {select, templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(bookingWidget){
+    const thisBooking = this;
+    
+    thisBooking.render(bookingWidget);
+    thisBooking.initWidgets();
+  }
+    
+  render(bookingWidget){
+    const thisBooking = this;
+    
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = bookingWidget;
+    bookingWidget.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = bookingWidget.querySelector(select.widgets.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = bookingWidget.querySelector(select.widgets.booking.hoursAmount);
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+  }
+
+}
+
+export default Booking;
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,11 +1,17 @@
+/* global Handlebars */
+
+
 export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product',
+    bookingWidget: '#template-booking-widget',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -25,7 +31,20 @@ export const select = {
       input: 'input.amount', 
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
+      hourPicker: {
+        wrapper: '.hour-picker',
+        input: 'input[name="hours"]',
+        output: '.output',
+      }
     },
+    booking: {
+      peopleAmount: '.people-amount',
+      hoursAmount: '.hours-amount',
+      tables: '.floor-plan .table',
+    },
+  },
+  nav: {
+    links: '.main-nav a',
   },
   cart: {
     productList: '.cart__order-summary',
@@ -56,6 +75,16 @@ export const classNames = {
   cart: {
     wrapperActive: 'active',
   },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  }
 };
   
 export const settings = {
@@ -71,10 +100,27 @@ export const settings = {
     url: '//localhost:3131',
     products: 'products',
     orders: 'orders',
+    bookings: 'bookings',
+    events: 'events',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
+  },
+  hours: {
+    open: 12,
+    close: 24,
+  },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
+  booking: {
+    tableIdAttribute: 'data-table',
   }
 };
   
 export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
 };
